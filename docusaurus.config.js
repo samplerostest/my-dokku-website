6c571ac79143863f4bb2079b17302689dc07cbdb
aravinthansankaran <aravinthan.iovate@gmail.com>
2024-01-08 11:42:00 +0530

    url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: '',
+  url: 'https://samplerostest.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/my-dokku-website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
