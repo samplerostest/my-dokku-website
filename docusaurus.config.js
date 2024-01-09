927e0fda74fb2a9f81c2be63de356e1d5c5f6f8d
aravinthansankaran <aravinthan.iovate@gmail.com>
2024-01-09 16:45:20 +0530

    commit

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'RoS Documentation',
-  tagline: 'robot operating system',
+  title: 'RoS Documentations',
+  tagline: 'robot operating systems',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
